06cb842f0eee451507481977ecd4f4b3b913f695
Julia Costa <juliacosta232005@gmail.com>
2024-03-21 21:10:43 +0000

    Added Chrono exercice

---
+++ Chronos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Chronos
+{
+    class Program
+    {
+        static void Main()
+        {
+            Stopwatch crono1 = new Stopwatch();
+            Stopwatch crono2 = new Stopwatch();
+
+        }
+    }
+}
