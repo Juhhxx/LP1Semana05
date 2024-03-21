145fc275771bf5152c471788d37144b563581cd1
Julia Costa <juliacosta232005@gmail.com>
2024-03-21 21:59:22 +0000

    Added print total value of the dice and fixed issue

--- RanDice/Program.cs
+++ RanDice/Program.cs
@@ -8,14 +8,17 @@ namespace RanDice
         {
             int numDice = int.Parse(args[0]);
             int randSeed = int.Parse(args[1]);
-            int total;
+            int total = 0;
 
             Random diceResult = new Random(randSeed);
 
             for (int i = 0; i < numDice; i++)
             {
-                total =+ diceResult.Next(1,7);
+                int dice = diceResult.Next(1,7);
+                total += dice;
             }
+
+            Console.WriteLine(total);
 
         }
     }
