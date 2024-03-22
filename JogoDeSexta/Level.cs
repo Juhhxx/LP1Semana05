830318e9f9008c050a85aa471a537f83e3fcc4ca
Julia Costa <juliacosta232005@gmail.com>
2024-03-22 17:16:03 +0000

    Created Level class constructor

--- JogoDeSexta/Level.cs
+++ JogoDeSexta/Level.cs
@@ -5,6 +5,12 @@ namespace JogoDeSexta
         int roomNum;
         int diff;
         Enemy[,] roomEnemy;
+        public Level(int roomNum, int diff)
+        {
+            this.roomNum = roomNum;
+            this.diff = diff;
+            roomEnemy = new Enemy[roomNum,1];
+        }
         
     }
 }
